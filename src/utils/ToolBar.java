0818b8c904ed3019800df5eb1e6aa881939358d2
Adrian WIlk <adrian.wilu@gmail.com>
2017-04-21 18:39:45 +0200

    Add files via upload

--- src/utils/ToolBar.java
+++ src/utils/ToolBar.java
@@ -1,63 +1,96 @@
 
 package utils;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JRootPane;
 import javax.swing.JToolBar;
 
 
 public class ToolBar {
     
-static final private String PREVIOUS = "previous";
-static final private String UP = "up";
-static final private String NEXT = "next";
-    
+
 public static JToolBar toolBar;
     
     JButton previous;
     JButton up;
+    JButton down;
     JButton next;
+    
     
 
     public ToolBar() {
-//         super(new BorderLayout());
+
     }
     
     public JToolBar getToolBar(JRootPane root){
         
-        toolBar= new JToolBar("TOOLS");       
-        
-        previous= new JButton("PREVIOUS");        
+        toolBar= new JToolBar("Navigation");       
+           
+         try{
+            BufferedImage buttonIcon1 = ImageIO.read(new File("toolbarButtonGraphics/navigation/Back24.gif"));
+            previous = new JButton(new ImageIcon(buttonIcon1));
+            }catch(Exception io){
+                 previous.setText("Previous");
+                 JOptionPane.showMessageDialog(null,io);
+            }  
         previous.addActionListener((e)->{
-                        
-            String description = "taken you to the previous <something>.";
-            displayResult("If this were a real app, it would have "
-                + description);
+            
+          displayResult("Previous");
             
         });
         toolBar.add(previous);
         
-        up= new JButton("UP");
+        
+        try{
+            BufferedImage buttonIcon2 = ImageIO.read(new File("toolbarButtonGraphics/navigation/Up24.gif"));
+            up = new JButton(new ImageIcon(buttonIcon2));
+            }catch(Exception io){
+                 up.setText("Up");
+                 JOptionPane.showMessageDialog(null,io);
+            }  
         up.addActionListener((e)->{
              
-            String description = "taken you to the Up <something>.";
-            displayResult("If this were a real app, it would have "
-                + description);
+        displayResult("Up");
             
         });
         toolBar.add(up);
         
-        next= new JButton("NEXT");
+          try{
+            BufferedImage buttonIcon3 = ImageIO.read(new File("toolbarButtonGraphics/navigation/Down24.gif"));
+            down = new JButton(new ImageIcon(buttonIcon3));
+            }catch(Exception io){
+                 down.setText("Down");
+                 JOptionPane.showMessageDialog(null,io);
+            }  
+        down.addActionListener((e)->{
+            
+          displayResult("Down");
+            
+        });
+        toolBar.add(down);
+        
+         try{
+            BufferedImage buttonIcon4 = ImageIO.read(new File("toolbarButtonGraphics/navigation/Forward24.gif"));
+            next = new JButton(new ImageIcon(buttonIcon4));
+            }catch(Exception io){
+                 next.setText("Next");
+                 JOptionPane.showMessageDialog(null,io);
+            }  
         next.addActionListener((e)->{
             
-            String description = "taken you to the Forward <something>.";
-            displayResult("If this were a real app, it would have "
-                + description);
+            
+            displayResult("Next");
+
             
         });
         toolBar.add(next);
-        
-        
         
         return toolBar;
     }
